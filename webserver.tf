89408c2c57afa4b2d87b7b97f6d81f8a864ca107
Mack Hendricks <mack@dopensource.com>
2021-04-22 13:05:28 -0400

    Updated

--- webserver.tf
+++ webserver.tf
@@ -21,7 +21,7 @@ resource "google_compute_instance" "webserver" {
  metadata_startup_script = "sudo apt-get update && apt-get install -y git nginx"
 
  network_interface {
-   network = "vpc-prod"
+   network = "vpc-dev"
 
    access_config {
      // Include this section to give the VM an external ip address
